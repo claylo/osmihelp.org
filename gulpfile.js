b8279531df395b9c14208d539804fbf8a7f0aafd
Ed Finkler <coj@funkatron.com>
2016-04-17 15:40:02 -0400

    Use Twig.js templates to make static html - installed gulp-twig - make .twig templates - new gulp tasks to watch and build twig->html

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ gulp.task('default', function() {
     // place code for your default task here
 });
 
+gulp.task('build', ['twig', 'less']);
+
 gulp.task('less', function () {
     return gulp.src('./public/less/style.less')
         .pipe(less())
@@ -20,9 +22,28 @@ gulp.task('mincss', function () {
         .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('twig', function () {
+    var twig = require('gulp-twig');
+    return gulp.src('./templates/pages/**/*.twig')
+        .pipe(twig({
+            'base': './templates/',
+            'cache': 'false'
+        }))
+        .pipe(gulp.dest('./public/'));
+});
+
 gulp.task('watch-less', ['less'], function() {
     var watcher = gulp.watch('./public/**/*.less', ['less']);
     watcher.on('change', function (event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 });
+
+gulp.task('watch-twig', ['twig'], function() {
+    var watcher = gulp.watch('./templates/**/*.twig', ['twig']);
+    watcher.on('change', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('watch', ['watch-twig','watch-less']);
