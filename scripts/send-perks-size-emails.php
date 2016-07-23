a5a108345c3cccd5e2548ae6a244d6c32aaed12c
Ed Finkler <coj@funkatron.com>
2016-07-23 19:55:58 -0400

    update script to use .env and send emails

--- scripts/send-perks-size-emails.php
+++ scripts/send-perks-size-emails.php
@@ -2,6 +2,7 @@
 
 require dirname(__FILE__) . '/../vendor/autoload.php';
 
+use Aura\Sql\ExtendedPdo;
 use GuzzleHttp\Client;
 use SparkPost\SparkPost;
 use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
@@ -17,26 +18,42 @@ $sparky = new SparkPost($httpAdapter, [
 
 $htmlTpl = file_get_contents(dirname(__DIR__) . "/templates/email/rewards-sizing-form-2016.tpl");
 
-$results = $sparky->transmissions->post([
-    'content' => [
-        'from' => [
-            'name' => 'OSMIHelp',
-            'email' => 'info@osmihelp.org',
+$pdo = new ExtendedPdo(
+    getenv('PDO_DB_CONNECTION'),
+    getenv('PDO_DB_USERNAME'),
+    getenv('PDO_DB_PASSWORD')
+);
+
+$stm = "SELECT * FROM `indiegogo` WHERE `Perk ID` = 3613098 OR `Perk ID` = 3633662";
+$sth = $pdo->query($stm);
+$rs = $sth->fetchAll(PDO::FETCH_ASSOC);
+if (!$rs) {
+    throw new Exception('No results for matching perk customers');
+}
+
+foreach ($rs as $row) {
+    echo "Sending to {$row['Email']}...\n";
+    $results = $sparky->transmissions->post([
+        'content' => [
+            'from' => [
+                'name' => 'OSMIHelp',
+                'email' => 'info@osmihelp.org',
+            ],
+            'subject' => 'OSMI Shirts and Hoodies: We need your size info!',
+            'html' => $htmlTpl,
         ],
-        'subject' => 'OSMI Shirts and Hoodies: We need your size info!',
-        'html' => $htmlTpl,
-    ],
-    'substitution_data' => ['name' => 'Ed', 'email' => 'coj@funkatron.com'],
-    'recipients' => [
-        [
-            'address' => [
-                'name' => 'Ed Finkler',
-                'email' => 'coj@funkatron.com',
+        'substitution_data' => ['name' => $row['Name'], 'email' => $row['Email']],
+        'recipients' => [
+            [
+                'address' => [
+                    'name' => $row['Name'],
+                    'email' => $row['Email'],
+                ],
             ],
         ],
-    ],
-    'cc' => null,
-    'bcc' => null,
-]);
+        'cc' => null,
+        'bcc' => null,
+    ]);
+}
 
-var_dump($results);
+echo "done.\n";
