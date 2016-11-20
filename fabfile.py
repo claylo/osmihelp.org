392469e7877f04ee7646f6d3f4e29c81ba6d98fd
Ed Finkler <coj@funkatron.com>
2016-11-20 11:44:01 -0500

    update fabfile deploy command to do full deploy

--- fabfile.py
+++ fabfile.py
@@ -7,9 +7,15 @@ def uname():
     run('uname')
 
 def deploy():
+    with cd('/var/www'):
+        run('sudo rm -rf osmihelp.org', warn_only=True)
+        run('sudo mkdir osmihelp.org')
+        run('sudo chown coj:coj osmihelp.org')
+        run('rm -rf osmihelp.org/*')
+        run('git clone https://github.com/OSMIHelp/osmihelp.org.git osmihelp.org')
     with cd('/var/www/osmihelp.org/'):
-        run('git pull')
         run('chmod 777 cache')
+        run('git pull')
         run('composer update --optimize-autoloader --no-dev')
         run('npm install')
         run('bower install')
@@ -17,4 +23,5 @@ def deploy():
 def update():
     with cd('/var/www/osmihelp.org/'):
         run('git pull')
+        run('rm cache/*')
         run('composer update --optimize-autoloader --no-dev')
