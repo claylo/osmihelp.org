3cd407a26ac3ccac0d25563bea23a1bfd680cd53
Ed Finkler <coj@funkatron.com>
2017-02-19 21:59:49 -0500

    get content converted to Statamic

---
+++ public-statamic/assets/js/app.js
@@ -0,0 +1,489 @@
+/**
+ * the data we use to generate the talk section
+ * @type {*[]}
+ */
+window.PRESENTATIONS = [
+    {
+        "ytid": 'e8OVyjtz1uc',
+        "video_url": 'https://www.youtube.com/watch?v=e8OVyjtz1uc',
+        "speakerdeck_embed_id": "e2b877c760b4455a98626adb5d2f5b09",
+        "speakerdeck_embed_ratio": "1.77777777777778",
+        "slides_url": "https://speakerdeck.com/funkatron/stronger-than-fear-mental-health-in-the-developer-community-20",
+        'event_name': 'LoopConf',
+        'event_date': 'February 2017',
+        "audio_url": null
+    },
+    {
+        "ytid": null,
+        "video_url": null,
+        "speakerdeck_embed_id": "1df06a7286014cf58acd411452ffd9fa",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmidevspace16slides",
+        'event_name': 'DevSpace Conference 2016',
+        'event_date': 'October 2016',
+        "audio_url": null
+    },
+    {
+        "ytid": null,
+        "video_url": null,
+        "speakerdeck_embed_id": "fd085d9a5346493cb6541de7eb8035f3",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmimadisonphp2016slides",
+        'event_name': 'Madison PHP Conference 2016',
+        'event_date': 'October 2016',
+        "audio_url": null
+    },
+    {
+        "ytid": null,
+        "video_url": null,
+        "speakerdeck_embed_id": "4b328a72f7bd43b9a913ed1fcb171335",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmipnwphp16slides",
+        'event_name': 'PNWPHP 2016',
+        'event_date': 'September 2016',
+        "audio_url": null
+    },
+    {
+        "ytid": 'E6hU2b0mg18',
+        "video_url": "http://j.mp/osmipyohio16video",
+        "speakerdeck_embed_id": "7d66191fcb79411c9c7e392ce1da84a4",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmipyohio16slides",
+        'event_name': 'PyOhio 2016',
+        'event_date': 'July 2016',
+        "audio_url": null
+    },
+    {
+        "ytid": 'bWqSuBg8AMo',
+        "video_url": "http://j.mp/osmiyapc16video",
+        "speakerdeck_embed_id": "fa13176df88b4692befd6e1499e97445",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmiyapcna2016slides",
+        "event_name": "The Perl Conference North America 2016",
+        "event_date": "June 2016",
+        "audio_url": null
+    },
+    {
+        "ytid": null,
+        "video_url": "https://videopress.com/v/df9jBrra",
+        "speakerdeck_embed_id": "3d814b5bf1364acb932fe7f182094e2a",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmiwcneo16slides",
+        "event_name": "WordCamp NEO",
+        "event_date": "June 2016",
+        "audio_url": null
+    },
+    {
+        "ytid": null,
+        "video_url": null,
+        "speakerdeck_embed_id": "d621c664fb58443e9a1465c3ec923cd2",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        "slides_url": "http://j.mp/osmiwcmpls16slides",
+        "event_name": "Minneapolis Wordcamp",
+        "event_date": "May 2016",
+        "audio_url": null
+    },
+    {
+        "ytid": "ytMHC_eq6nI",
+        "video_url": "http://j.mp/osmikalxvideo",
+        "slides_url": "http://j.mp/osmikalx16slides",
+        "event_name": "Kalamazoo X 2016",
+        "event_date": "April 2016",
+        "audio_url": null
+    },
+    {
+        "ytid": null,
+        "video_url": null,
+        "speakerdeck_embed_id": "aa971ab9d8e242d49223e1e728f78e18",
+        "speakerdeck_embed_ratio": "1.33333333333333",
+        'slides_url': 'http://j.mp/osmithecombine2016slides',
+        'event_url': 'http://thecombine.org',
+        'event_name': 'The Combine',
+        'event_date': 'April 2016',
+        "audio_url": null
+    },
+    {
+        "ytid": "9VcDzjeNfqQ",
+        "video_url": "http://j.mp/osmiskiphp16video",
+        "slides_url": "http://j.mp/osmiskiphp16slides",
+        "event_name": "SkiPHP 2016",
+        "event_date": "January 2016",
+        "audio_url": null
+    },
+    {
+        "ytid": "80IMTbZv8-I",
+        "video_url": "http://j.mp/osminodevember2015video",
+        "slides_url": "http://j.mp/osminodevember2015slides",
+        "event_name": "Nodevember 2015",
+        "event_date": "November 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "gvDdvrqOahM",
+        "video_url": "http://j.mp/osmitnphp15video",
+        "slides_url": "http://j.mp/osmitnphp15slides",
+        "event_name": "True North PHP 2015",
+        "event_date": "November 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "KPQs0GwVvnQ",
+        "video_url": "http://j.mp/osmiconnectjs15video",
+        "slides_url": "http://j.mp/osmiconnectjs15slides",
+        "event_name": "Connect-JS 2015",
+        "event_date": "October 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "TSnhs_2zZhQ",
+        "video_url": "http://j.mp/osmidwconf15video",
+        "slides_url": "http://j.mp/osmidwconf15slides",
+        "event_name": "DevWorkshop Conference 2015",
+        "event_date": "October 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "PPNaKiO5oFQ",
+        "video_url": "http://j.mp/osmipnwphp2015video",
+        "slides_url": "http://j.mp/osmipnwphp2015slides",
+        "event_name": "Pacific Northwest PHP Conference 2015",
+        "event_date": "September 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "Fn0MlchshUU",
+        "video_url": "http://j.mp/1MKLMfd",
+        "slides_url": "http://j.mp/osminephp15slides",
+        "event_name": "Northeast PHP Conference 2015",
+        "event_date": "August 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "0DFh5n8VeBo",
+        "video_url": "http://j.mp/1MODkwW",
+        "slides_url": "http://j.mp/osmilara15slides",
+        "event_name": "Laracon US 2015",
+        "event_date": "August 2015",
+        "audio_url": null
+    },
+    {
+        "ytid": "anpVOGvsym8",
+        "video_url": "http://j.mp/osmiosb15video",
+        "slides_url": "http://j.mp/osmiosb15slides",
+        "event_name": "Open Source Bridge 2015",
+        "event_date": "June 2015",
+        "audio_url": null
+    },
+
+    {
+        "ytid": "C_ICrD4BZes",
+        "video_url": "http://j.mp/stfpytn15video",
+        "slides_url": "http://j.mp/stfpytn15slides",
+        "event_name": "PyTennesee 2015",
+        "event_date": "February 2015",
+        "audio_url": null
+    },
+
+    {
+        "ytid": "L_jLBKa1dYE",
+        "video_url": "http://j.mp/osmitnphp14video",
+        "slides_url": "http://j.mp/osmitnphp14slides",
+        "event_name": "True North PHP 2014",
+        "event_date": "November 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "Ap7MMlw8gzE",
+        "video_url": "http://j.mp/osmidevworkshopvideo",
+        "slides_url": "http://j.mp/osmidevworkshopslides",
+        "event_name": "DevWorkshop Conf 2014",
+        "event_date": "September 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "YX12UxsFEYg",
+        "video_url": "http://j.mp/osmimadisonphpvideo",
+        "slides_url": "http://j.mp/osmimadisonphp",
+        "event_name": "MadisonPHP 2014",
+        "event_date": "September 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "4tswmjhA7Ws",
+        "video_url": "http://j.mp/osmioscon14video",
+        "slides_url": "http://j.mp/osmioscon14slides",
+        "event_name": "OSCON 2014",
+        "event_date": "July 2014",
+        "audio_url": null
+    },
+
+
+
+    {
+        "ytid": "kfS_U7akhzo",
+        "video_url": "http://j.mp/osmiosb14video",
+        "slides_url": "http://j.mp/osmiosb14slides",
+        "event_name": "Open Source Bridge 2014",
+        "event_date": "June 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "vgwqBWzy6RE",
+        "video_url": "http://j.mp/osmiseaphpvideo",
+        "slides_url": "http://j.mp/osmiseaphpslides",
+        "event_name": "Seattle PHP Users Group",
+        "event_date": "April 11, 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "MnrsrCD7U40",
+        "video_url": "http://j.mp/osmipytn14video",
+        "slides_url": "http://j.mp/osmipytn14slides",
+        "event_name": "PyTennesee 2014",
+        "event_date": "February 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "nkFaCWEmXF4",
+        "video_url": "http://j.mp/osmiskiphp14video",
+        "slides_url": "http://j.mp/osmiskiphp14slides",
+        "event_name": "SkiPHP 2014",
+        "event_date": "January 2014",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "nkFaCWEmXF4",
+        "video_url": "http://j.mp/osmitnphp13video",
+        "slides_url": "http://j.mp/osmitnphp13slides",
+        "event_name": "True North PHP 2013",
+        "event_date": "November 2013",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "g_ku2YmnWkA",
+        "video_url": "http://j.mp/osmimadruby13video",
+        "video_url_alternamte": "//www.youtube-nocookie.com/embed/o-z1lY6BEZU",
+        "slides_url": "http://j.mp/osmimadruby13slides",
+        "event_name": "Madison Ruby 2013",
+        "event_date": "August 2013",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "DqRPln8X1sc",
+        "video_url": "http://j.mp/osmidistill13video",
+        "slides_url": "http://j.mp/osmidistill13slides",
+        "event_name": "Distill 2013",
+        "event_date": "August 2013",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "QqBl3ICJltc",
+        "video_url": "http://j.mp/osmivideooscon13",
+        "slides_url": "http://j.mp/osmislidesoscon13",
+        "event_name": "OSCON 2013",
+        "event_date": "July 2013",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "t0okOTvXfCY",
+        "video_url": "http://j.mp/osmivideolsp13",
+        "slides_url": "http://j.mp/osmislideslsp13",
+        "event_name": "Lone Star PHP 2013",
+        "event_date": "June 2013",
+        "audio_url": null
+    },
+
+
+    {
+        "ytid": "91qp1aAqRjY",
+        "video_url": "http://bit.ly/osmivideoosb13",
+        "slides_url": "http://bit.ly/osmislidesosb13",
+        "event_name": "Open Source Bridge 2013",
+        "event_date": "June 2013",
+        "audio_url": null
+    },
+
+    {
+        "ytid": null,
+        "video_url": null,
+        "slides_url": "http://bit.ly/osmislides",
+        "event_name": "php|tek 13",
+        "event_date": "May 2013",
+        "audio_url": "http://funkatron-random.s3.amazonaws.com/Open-Sourcing-Mental-Illness_tek13.mp3"
+    }
+];
+
+window.UPCOMING_PRESENTATIONS = [
+    {
+        'event_url': 'http://www.elixirdaze.com/',
+        'event_name': 'Elixir Daze',
+        'event_date': 'March 2-3',
+        'event_location': 'St. Augustine, Florida'
+    },
+    {
+        'event_url': 'http://lonestarphp.com/',
+        'event_name': 'Lone Star PHP',
+        'event_date': 'April 20-22',
+        'event_location': 'Dallas, Texas'
+    },
+    {
+        'event_url': 'https://cakefest.org/',
+        'event_name': 'CakeFest 2017',
+        'event_date': 'June 8-11',
+        'event_location': 'New York City, New York'
+    },
+];
+
+
+/**
+ * config stuff
+ * we have to change the delimiters so twig tags don't clash
+ */
+Vue.config.debug = true;
+Vue.config.delimiters = ["${", "}"];
+Vue.config.unsafeDelimiters = ['{!!', '!!}'];
+
+var talksSection = new Vue({
+    el: '#app',
+    components: {
+        'alert': VueStrap.alert,
+        'presentation-item': {
+            template: '#presentation-item-tpl',
+            props: ['event_name', 'event_date', 'slides_url', 'video_url', 'ytid', 'audio_url',
+                'speakerdeck_embed_id', 'speakerdeck_embed_ratio'],
+            data: function () {
+                return {};
+            },
+            methods: {
+                viewPresentation: function () {
+                    this.$dispatch('view-presentation', {
+                        'event_name': this.event_name,
+                        'event_date': this.event_date,
+                        'slides_url': this.slides_url,
+                        'video_url': this.video_url,
+                        'ytid': this.ytid,
+                        'audio_url': this.audio_url,
+                        "speakerdeck_embed_id": this.speakerdeck_embed_id,
+                        "speakerdeck_embed_ratio": this.speakerdeck_embed_ratio
+                    });
+                    $.scrollTo('#talks', 200);
+
+                }
+            }
+        },
+        'presentation-player': {
+            template: '#presentation-player-tpl',
+            props: ['event_name', 'event_date', 'slides_url', 'video_url', 'ytid', 'audio_url',
+                'speakerdeck_embed_id', 'speakerdeck_embed_ratio'],
+            data: function () {
+                return {};
+            }
+        },
+        'upcoming-presentation-item': {
+            template: '#upcoming-presentation-item-tpl',
+            props: ['event_name', 'event_date', 'event_location', 'event_url']
+        }
+    },
+    data: {
+        // just reference the data set up above
+        selectedPresentation: {
+            "ytid": null,
+            "video_url": null,
+            "speakerdeck_embed_id": null,
+            "speakerdeck_embed_ratio": null,
+            "slides_url": null,
+            "event_name": null,
+            "event_date": null,
+            "audio_url": null
+        },
+        presentations: window.PRESENTATIONS,
+        upcomingPresentations: window.UPCOMING_PRESENTATIONS
+    },
+
+    events: {
+        /**
+         *
+         * @param presentationData
+         */
+        'view-presentation': function (presentationData) {
+            this.$refs.selectedPresentationComponent.speakerdeck_embed_id = presentationData.speakerdeck_embed_id;
+            this.$refs.selectedPresentationComponent.speakerdeck_embed_ratio = presentationData.speakerdeck_embed_ratio;
+            this.$refs.selectedPresentationComponent.event_name = presentationData.event_name;
+            this.$refs.selectedPresentationComponent.event_date = presentationData.event_date;
+            this.$refs.selectedPresentationComponent.slides_url = presentationData.slides_url;
+            this.$refs.selectedPresentationComponent.video_url = presentationData.video_url;
+            this.$refs.selectedPresentationComponent.ytid = presentationData.ytid;
+            this.$refs.selectedPresentationComponent.audio_url = presentationData.audio_url;
+        }
+    }
+
+});
+
+// initialize selected presentation;
+talksSection.selectedPresentation = talksSection.presentations[0];
+
+var OSMI = {
+    addDisclaimer: function(copySelector, cookieKey) {
+        if (Cookies.get(cookieKey) !== 'true') {
+            bootbox.dialog({
+                size: 'large',
+                message: "<p>" + $(copySelector).html() + "</p>",
+                title: "Disclaimer",
+                buttons: {
+                    disagree: {
+                        label: "I do not agree.",
+                        className: "btn-danger",
+                        callback: function() {
+                            window.location = "/resources/";
+                        }
+                    },
+                    agree: {
+                        label: "I agree, continue",
+                        className: "btn-success",
+                        callback: function() {
+                            Cookies.set(cookieKey, 'true');
+                        }
+                    }
+                }
+            });
+        }
+    },
+    openVolunteerModal: function(e) {
+        $ct = $(e.currentTarget);
+        var name = $ct.find('.volunteer-name').get(0).innerText;
+        var photo = $ct.find('.volunteer-photo').get(0).src;
+        var bio = $ct.find('.volunteer-bio').get(0).innerHTML;
+        bootbox.alert({
+            title: name,
+            message: "<img class='img-thumbnail img-block clearfix pull-right' style='width:50%' src='" + photo + "'><p>" + bio + "</p><div class='clearfix'></div>",
+            onEscape: true,
+        });
+
+    }
+}
+
+$(document).ready(function() {
+    $('.volunteer-thumbnail').bind('click', OSMI.openVolunteerModal);
+});
